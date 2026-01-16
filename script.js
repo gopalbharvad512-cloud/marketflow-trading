e603f38a4c0e0cb156a534af15892f1de7100016
gopalbharvad512-cloud <gopalbharvad512@gmail.com>
2026-01-16 10:44:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-window.onload = function(){
-setTimeout(()=>{
-document.getElementById("loader").style.display="none";
-document.getElementById("content").style.display="block";
-},1200);
-}
-function toggleMenu(){
-document.getElementById("nav").classList.toggle("active");
-}
+window.onload=()=>{document.getElementById("loader").style.display="none"};
