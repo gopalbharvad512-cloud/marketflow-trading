9e7e004a5fff7212f5ea900f9baab7a552f9c99e
gopalbharvad512-cloud <gopalbharvad512@gmail.com>
2026-01-16 11:58:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-window.onload=()=>{
-document.getElementById("loader").style.display="none";
-};
-function toggleMenu(){
-document.getElementById("nav").classList.toggle("active");
-}
-function changePair(){
-var pair=document.getElementById("pairSelect").value;
-document.getElementById("chartFrame").src=
-"https://s.tradingview.com/widgetembed/?theme=dark&interval=15&symbol="+pair;
-}
+console.log('MarketFlow Loaded');
