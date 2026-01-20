0fbd73a7a7f71eac057a7f3f6df36b9283f67f46
gopalbharvad512-cloud <gopalbharvad512@gmail.com>
2026-01-20 08:20:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Market Flow Live loaded');
+let currentPair = 'BINANCE:BTCUSDT';
+let currentTF = '15';
+
+function updateChart(){
+document.getElementById('tvChart').src =
+`https://s.tradingview.com/widgetembed/?symbol=${currentPair}&interval=${currentTF}&theme=dark`;
+}
+
+function changePair(){
+currentPair = document.getElementById('pairSelect').value;
+updateChart();
+}
+
+function changeTF(tf){
+currentTF = tf;
+document.querySelectorAll('.timeframes button').forEach(b=>b.classList.remove('active'));
+event.target.classList.add('active');
+updateChart();
+}
